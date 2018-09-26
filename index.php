76571498fc1d52e06d31fc3f8a7c7198d6b869ed
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-09-26 14:29:58 -0600

    updated

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php require_once("main_page/main_page.html"); ?>
+<?php require_once("page_1/page_1.html"); ?>
+<?php require_once("page_2/page_2.html"); ?>
+<?php require_once("page_3/page_3.html"); ?>
+<?php require_once("page_4/page_4.html"); ?>
+<?php require_once("admin_page/admin.html"); ?>
+
