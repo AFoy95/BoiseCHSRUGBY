d72e062db7fdcfbd21363c3fbcfe3776ebd82643
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-09-26 19:37:46 -0600

    hello there

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
 <?php require_once("main_page/main_page.html"); ?>
-<?php require_once("page_1/page_1.html"); ?>
-<?php require_once("page_2/page_2.html"); ?>
-<?php require_once("page_3/page_3.html"); ?>
-<?php require_once("page_4/page_4.html"); ?>
-<?php require_once("admin_page/admin.html"); ?>
 
