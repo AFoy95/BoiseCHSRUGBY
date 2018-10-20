01e35152b2d60e6beb0775c02547acac0c6ddd05
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-10-19 23:54:02 -0600

    php

--- myfile.sql
+++ myfile.sql
@@ -2,3 +2,26 @@ CREATE TABLE admin(
 	email VARCHAR(256) NOT NULL PRIMARY KEY,
 	password VARCHAR (128)NOT NULL
 );
+
+CREATE TABLE Boys_Scores(
+	Our_Score Integer(150) NOT NULL
+	Their_Score Integer(150) NOT NULL
+	Penalties Integer(25) NOT NULL
+	Convmade Integer(25) NOT NULL
+	Convatt Integer(25) NOT NULL
+);
+
+CREATE TABLE Girls_Scores(
+        Our_Score Integer(150) NOT NULL
+        Their_Score Integer(150) NOT NULL
+        Penalties Integer(25) NOT NULL
+        Convmade Integer(25) NOT NULL
+        Convatt Integer(25) NOT NULL
+);
+
+CREATE TABLE Schedule(
+	Dates VARCHAR(256)
+	Events VARCHAR(256)
+);
+
+
