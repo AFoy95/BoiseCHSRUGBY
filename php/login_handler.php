f11b6bfd1262888fbed86267289d33e6f022ed79
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-10-28 21:56:03 -0600

    hello

--- php/login_handler.php
+++ php/login_handler.php
@@ -1,16 +1,10 @@
 <?php
-require_once "Dao.php";
 session_start();
-$dao=new Dao();
-$id=__GET("id");
 
   if ($login == 'Foy' && $password == 'CHEMAN5') {
-    $_SESSION['logged_in'] = true;
     header('/html/admin.html');
     exit;
   }
-  $_SESSION['logged_in'] = false;
-  $_SESSION['message'] = "Username or password invalid";
   header('/php/login.php');
   exit;
 ~
