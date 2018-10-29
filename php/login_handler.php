dabf9af5abc40f4aeae963037788c405f0ef783d
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-10-28 21:47:09 -0600

    login_handler.php

--- php/login_handler.php
+++ php/login_handler.php
@@ -2,17 +2,16 @@
 require_once "Dao.php";
 session_start();
 $dao=new Dao();
-$id=$_GET["id"];
-
+$id=__GET("id");
 
   if ($login == 'Foy' && $password == 'CHEMAN5') {
     $_SESSION['logged_in'] = true;
-    header('Location: https://boisechsrugby.herokuapp.com/admin.html');
+    header('/html/admin.html');
     exit;
   }
   $_SESSION['logged_in'] = false;
   $_SESSION['message'] = "Username or password invalid";
-  header('https://boisechsrugby.herokuapp.com/');
+  header('/php/login.php');
   exit;
 ~
 ~
