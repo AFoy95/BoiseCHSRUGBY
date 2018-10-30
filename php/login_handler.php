68aa405f43fc8843831ee066cb040d5f5b6c3601
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-10-29 20:30:11 -0600

    DAO

--- php/login_handler.php
+++ php/login_handler.php
@@ -1,11 +1,5 @@
 <?php
  session_start();
- require 'Dao.php';
- $dao=new Dao();
- $conn=$dao->getConnection();
  echo "hello";
- header 'https://www.boisechsrugby.herokuapp.com/admin/admin.html';
- exit;
- 
 ?>
 	
