171719943ef6bc278211729a5f08ac4a3e7ca1b2
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-10-29 22:07:55 -0600

    post not working

--- php/login_handler.php
+++ php/login_handler.php
@@ -6,7 +6,8 @@
  $conn=$dao->getConnection();
  $username = $_POST["username"];
  $password = $_POST["password"];
-	if($username == dao->get_admin_user(1) && dao->get_admin_pass(1)){
+ $id=1;
+	if($username == dao->get_admin_user($id) && dao->get_admin_pass($id)){
 		header('Location: https://boisechsrugby.herokuapp.com/admin_page/admin.html');
 		exit;
 	}
