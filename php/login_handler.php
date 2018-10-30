e0b0824b695b2338cfde208d322003c2ee22232c
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-10-29 22:11:29 -0600

    login_handler

--- php/login_handler.php
+++ php/login_handler.php
@@ -6,7 +6,7 @@
  $conn=$dao->getConnection();
  $username = $_POST["username"];
  $password = $_POST["password"];
- $id=1;
+ $id="1";
 	if($username == dao->get_admin_user($id) && dao->get_admin_pass($id)){
 		header('Location: https://boisechsrugby.herokuapp.com/admin_page/admin.html');
 		exit;
