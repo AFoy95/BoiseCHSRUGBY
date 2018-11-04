4905f6fc39f9702c6354f4a55c85e021e8a75755
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-04 11:57:56 -0700

    adjust

--- php/login_handler.php
+++ php/login_handler.php
@@ -6,10 +6,11 @@
  $conn=$dao->getConnection();
  $username = $_POST['username'];
  $password = $_POST['password'];
-	if($dao->get_admin_user($username) && $dao->get_admin_pass($password) == 1){
+	if($dao->get_admin_user($username) && $dao->get_admin_pass($password)){
 		header('Location: https://boisechsrugby.herokuapp.com/admin_page/admin.html');
 		exit;
 	}
+	$_SESSION['message'] = "Invalid Username and/or Password";
 	header('Location: https://boisechsrugby.herokuapp.com/php/login.php');
 	exit;
 	
