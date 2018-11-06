87d20c89a750f8232b283fce39ffb93f8ff51ca0
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-05 21:49:43 -0700

    implementing php

---
+++ php/Schedules.php
@@ -0,0 +1,19 @@
+<?php
+	include'Dao.php';
+	session_start();
+?>
+
+<html>
+  <header><title>Schedules</title></header>
+  <head>
+  <link href="/css/Schedules.css" type="text/css" rel="stylesheet" />
+  </head>
+  		<h1>Schedules</h1>
+  <body>
+	  <table><th>Dates</th><th>Events</th>
+		  <tr><td>sept 26</td><td>hw4</td></tr>
+		  <tr><td>sept 30</td><td>World Domination</td></tr>
+	</table>
+
+  </body>
+</html>
