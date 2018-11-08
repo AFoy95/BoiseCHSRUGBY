523fdef704b15e5876aaf7266faa236335ca3abf
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-08 02:32:57 -0700

    updating Css

--- php/About_Us.php
+++ php/About_Us.php
@@ -8,7 +8,6 @@
   <head>
   <link href="/css/About_Us.css" type="text/css" rel="stylesheet" />
   </head>
-				<body id="background">
 						<div id="header">
                                 <h1>CHS<img src="/photos/logo.PNG"/>RUGBY</h1>
 									<?php if($_Session['logged_in']){
@@ -26,6 +25,7 @@
 							</div>
                                 </div>		
  
+	<body id="background">
 	  Hello all, This will be a basic page, that will include a basic
 	  about us in the program. am writing something. Yes, I plan to make it the most boring thing ever written. I go to the store. A car is parked. Many cars are parked or moving. Some are blue. Some are tan. They have windows. In the store, there are items for sale. These include such things as soap, detergent, magazines, and lettuce. You can enhance your life with these products. Soap can be used for bathing, be it in a bathtub or in a shower. Apply the soap to your body and rinse. Detergent is used to wash clothes. Place your dirty clothes into a washing machine and add some detergent as directed on the box. Select the appropriate settings on your washing machine and you should be ready to begin. Magazines are stapled reading material made with glossy paper, and they cover a wide variety of topics, ranging from news and politics to business and stock market information. Some magazines are concerned with more recreational topics, like sports card collecting or different kinds of hairstyles. Lettuce is a vegetable. It is usually green and leafy, and is the main ingredient of salads. You may have an appliance at home that can quickly shred lettuce for use in salads. Lettuce is also used as an optional item for hamburgers and deli sandwiches. Some people even eat lettuce by itself. I have not done this. So you can purchase many types of things at stores. -The Bradezone
   </body>
