16bb44b98f9225951fa96681582c6d1840a37825
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-08 02:05:12 -0700

    updating Css

--- php/main_page.php
+++ php/main_page.php
@@ -21,8 +21,13 @@ session_start();
 												<a href=/php/logout.php id=logout> LOGOUT</a> 
 									";}?>
 									
-								
-								
+						<div class="topnav">
+						<a class="active" href="/php/main_page.php">Main-page</a>
+						<a href="/html/About_Us.html">About-Us</a>
+						<a href="/html/Personal_Bios.html">Personal-Bios</a>	
+						<a href="/php/Schedules.php">Schedule</a>	
+						<a href="/php/Tables.php">Tables</a>
+						<a href="/php/login.php">Admin Login</a>
                         </div>
                                 <div>
                                         <div id="navigation">
