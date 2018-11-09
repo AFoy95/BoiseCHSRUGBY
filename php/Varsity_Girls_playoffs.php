32543f85bfbb514fe4a3e7acaeaf15b69935e32e
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-08 18:54:01 -0700

    updating Dao

---
+++ php/Varsity_Girls_playoffs.php
@@ -0,0 +1,150 @@
+<?php
+	include'Dao.php';
+	session_start();
+	$dao=new Dao();
+	$conn=$dao->getConnection();
+?>
+<html>
+  <header><title>Scores</title></header>
+  <head>
+  <link href="/css/tables.css" type="text/css" rel="stylesheet" />
+  </head>
+  <div id="header">
+                                <h1>CHS<img src="/photos/logo.PNG"/>RUGBY</h1>
+									
+							
+							<div class="topnav">
+								<a href="/php/main_page.php">Main-page</a>
+								<a href="/php/About_Us.php">About-Us</a>
+								<a href="/php/Personal_Bios.php">Personal-Bios</a>	
+								<a href="/php/Schedules.php">Schedule</a>	
+								<a class="active" href="/php/Tables.php">Tables</a>
+								<a href="/php/login.php">Admin Login</a>
+								<?php if($_SESSION['logged_in']){
+											echo" 
+								<a href=/php/logout.php> LOGOUT</a> 
+									";}?>
+							</div>
+                                </div>	
+  
+  <body>
+	   <div class ="sidenav">
+		<div class = "dropdown">
+		<button class="dropbtn">Boys Games</button>
+		<div class = "dropdown-content">
+		<a href="#">Regular Season</a>
+		<a href="#">Playoffs</a>
+		</div>
+		</div>
+		<div class = "dropdown">
+		<button class="dropbtn">Girls Games</button>
+		<div class = "dropdown-content">
+		<a href="#">Regular Season</a>
+		<a href="#">Playoffs</a>
+		</div>
+		</div>
+		<div class = "dropdown">
+		<button class="dropbtn">Jv Boys Games</button>
+		<div class = "dropdown-content">
+		<a href="#">Regular Season</a>
+		<a href="#">Playoffs</a>
+		</div>
+		</div>
+		</div>
+	  <table>
+		  <h2>Boys Score</h2>
+		  	<tr><td>Game #</td><td>Date</td><td>Capital</td><td>Capital Score</td><td>Opponent</td><td>Opp Score</td><td>Conversion Made</td><td>Conversion Attempt</td><td>Penalties</td></tr>
+	<?php 
+			
+		?>
+
+	  </table>
+				
+				<?php if($_SESSION['logged_in']){
+					$id;
+					$our_score;
+					$their_score;
+					$penalties;
+					$convmade;
+					$convatt;
+					$our_name;
+					$their_name;
+					$dates;
+					echo "<div>Insert Boys Stats </div>";
+					 echo"
+						  <form action = Tables.php method=post>
+						   <br>Game Number:	<input type=text name=game_number id=game_number/></br>
+						  <br>Our Score:	<input type=text name=our_score id=our_score/></br>
+						  <br> Their Score:	<input type=text name=their_score id=their_score/></br>
+						  <br> Penalties:	<input type=text name=penalties id=penalties/> </br>
+						  <br> Conversion Made:	<input type=text name=convmade id=convmade/></br>
+						  <br> Conversion Attempts:	<input type=text name=convatt id=convatt/></br>
+						  <br> Our Team:	<input type=text name=our_name id=our_name/></br>
+						  <br> Their Team:	<input type=text name=their_name id=their_name/></br>
+						  <br> date:	<input type=text name=dates id=dates/></br>
+						  <br><input type=submit value=Submit /></br></form>";
+						  
+						$id=$_POST['game_number'];
+						$our_score=$_POST['our_score'];
+						$their_score=$_POST['their_score'];
+						$penalties=$_POST['penalties'];
+						$convmade=$_POST['convmade'];
+						$convatt=$_POST['convatt'];
+						$our_name=$_POST['our_name'];
+						$their_name=$_POST['their_name'];
+						$dates=$_POST['dates'];
+					$dao->boys_Stats($id,$dates,$our_name,$our_score,$their_name,$their_score,$convmade,$convatt,$penalties);
+				}?>
+		
+  	
+  
+ <table>
+		<h3> Girls Score</h3>        
+                        <tr><td>Game #</td><td>Date</td><td>Capital</td><td>Capital Score</td><td>Opponent</td><td>Opp Score</td><td>Conversion Made</td><td>Conversion Attempt</td><td>Penalties</td></tr>
+
+          </table>
+		
+  <?php if($_SESSION['logged_in']){
+					$our_score;
+					$their_score;
+					$penalties;
+					$convmade;
+					$convatt;
+					$our_name;
+					$their_name;
+					$dates;
+					echo "<div>Insert Girls Stats </div>";
+					 echo"
+						  <form action = Tables.php method=post>
+						   <br>Game number:	<input type=text name=game_number id=game_number/></br>
+						  <br>Our Score:	<input type=text name=our_score id=our_score/></br>
+						  <br> Their Score:	<input type=text name=their_score id=their_score/></br>
+						  <br> Penalties:	<input type=text name=penalties id=penalties/> </br>
+						  <br> Conversion Made:	<input type=text name=convmade id=convmade/></br>
+						  <br> Conversion Attempts:	<input type=text name=convatt id=convatt/></br>
+						  <br> Our Team:	<input type=text name=our_name id=our_name/></br>
+						  <br> Their Team:	<input type=text name=their_name id=their_name/></br>
+						  <br> date:	<input type=text name=dates id=dates/></br>
+						  <br><input type=submit value=Submit /></br></form>";
+						  
+						 $id=$_POST['game_number']; 
+						$our_score=$_POST['our_score'];
+						$their_score=$_POST['their_score'];
+						$penalties=$_POST['penalties'];
+						$convmade=$_POST['convmade'];
+						$convatt=$_POST['convatt'];
+						$our_name=$_POST['our_name'];
+						$their_name=$_POST['their_name'];
+						$dates=$_POST['dates'];
+					$dao->girls_Stats($id,$dates,$our_name,$our_score,$their_name,$their_score,$convmade,$convatt,$penalties);
+				}?>
+  
+  
+  </body>
+ <div class="footer">
+  <p> &copy; boisechsrugby.com</p>
+  <p>&emsp;Admin: Austin Foy</p>
+  <p>&emsp;Contact information: <a href="mailto:afoy95@live.com">
+  afoy95@live.com</a>.</p>
+</div>
+</html>
