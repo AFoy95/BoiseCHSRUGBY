f6f2aa9f09b934c0ff735615f054b25dbcc02660
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-09 13:44:04 -0700

    css changes

--- php/JVarsity_Boys_playoffs.php
+++ php/JVarsity_Boys_playoffs.php
@@ -52,7 +52,8 @@
 		</div>
 		</div>
 		</div>
-  </div>
+	</div>
+   <div id = "table">
    <table>
 		  <h2>Jv Boys Playoff Scores</h2>
 		  	<tr><td>Game #</td><td>Date</td><td>Capital</td><td>Capital Score</td><td>Opponent</td><td>Opp Score</td><td>Conversion Made</td><td>Conversion Attempt</td><td>Penalties</td></tr>
@@ -97,6 +98,7 @@
 						$dates=$_POST['dates'];
 					$dao->boys_Stats($id,$dates,$our_name,$our_score,$their_name,$their_score,$convmade,$convatt,$penalties);
 				}?>
+			</div id = "table">
  <div class="footer">
   <p> &copy; boisechsrugby.com</p>
   <p>&emsp;Admin: Austin Foy</p>
