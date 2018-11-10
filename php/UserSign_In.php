0779cb3725883b722a37134b2570311a2f873ee7
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-09 17:31:49 -0700

    css changes

--- php/UserSign_In.php
+++ php/UserSign_In.php
@@ -5,12 +5,12 @@
  $conn=$dao->getConnection();
  $email = $_POST['email'];
  $password = $_POST['password'];
-	if($dao->get_admin_user($username) && $dao->get_admin_pass($password) || $_SESSION['logged_in']){
+	if($dao->get_User_email($username) && $dao->get_User_pass($password) || $_SESSION['logged_in']){
 		header('Location: https://boisechsrugby.herokuapp.com/');
 		$_SESSION['logged_in'] = true;
 		exit;
 	}
 	$_SESSION['message'] = "Invalid Username and/or Password";
 	$_SESSION['logged_in']=false;
-	header('Location: https://boisechsrugby.herokuapp.com/php/login.php');
+	header('Location: https://boisechsrugby.herokuapp.com/php/UserSign_Up.php');
 	exit;
