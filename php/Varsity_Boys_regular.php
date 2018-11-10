7b19912894cb5c7174f48fc86681207be2d51674
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-10 12:45:42 -0700

    implementing dynamic tables

--- php/Varsity_Boys_regular.php
+++ php/Varsity_Boys_regular.php
@@ -106,7 +106,7 @@
 						$our_name=$_POST['our_name'];
 						$their_name=$_POST['their_name'];
 						$dates=$_POST['dates'];
-					$dao->girls_Stats($id,$dates,$our_name,$our_score,$their_name,$their_score,$convmade,$convatt,$penalties);
+					$dao->boys_Stats($id,$dates,$our_name,$our_score,$their_name,$their_score,$convmade,$convatt,$penalties);
 				}?>
   </div>
  <div class="footer">
