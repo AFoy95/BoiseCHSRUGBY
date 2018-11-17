65cc0d1ae9637dc421f78e6bdd5876b9cdcb28cc
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-17 00:16:22 -0700

    adding js to top-level php

--- php/login_handles/login.php
+++ php/login_handles/login.php
@@ -69,5 +69,20 @@ unset($_SESSION['message']);
   <p>&emsp;Contact information: <a href="mailto:afoy95@live.com">
   afoy95@live.com</a>.</p>
 </div>
+	<script>
+		var dropdown = document.getElementsByClassName("dropbtn");
+		var i;
+			for(i=0;i<dropdown.length;i++){
+				dropdown[i].addEventListener("click",function(){
+					this.classList.toggle("active");
+					var dropdownContent=this.nextElementSibling;
+					if(dropdownContent.style.display === "block"){
+						dropdownContent.style.display="none";
+					}else{
+						dropdownContent.style.display="block";
+					}
+				});
+			}
+	  </script>
 </body>
 </html
