9cd09c9980402508e8b6e9afbbbb15d25bd97390
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-17 12:51:27 -0700

    added userPattern.js to my project

--- js/userPattern.js
+++ js/userPattern.js
@@ -17,7 +17,7 @@ myInput.onblur = function() {
 // When the user starts to type something inside the password field
 myInput.onkeyup = function() {
   // Validate lowercase letters
-  var lowerCaseLetters = "/[a-z]/g";
+  var lowerCaseLetters = /[a-z]/g;
   if(myInput.value.match(lowerCaseLetters)) { 
     letter.classList.remove("invalid");
     letter.classList.add("valid");
@@ -27,7 +27,7 @@ myInput.onkeyup = function() {
 }
 
   // Validate capital letters
-  var upperCaseLetters = "/[A-Z]/g";
+  var upperCaseLetters = /[A-Z]/g;
   if(myInput.value.match(upperCaseLetters)) { 
     capital.classList.remove("invalid");
     capital.classList.add("valid");
@@ -37,7 +37,7 @@ myInput.onkeyup = function() {
   }
 
   // Validate numbers
-  var numbers = "/[0-9]/g";
+  var numbers = /[0-9]/g;
   if(myInput.value.match(numbers)) { 
     number.classList.remove("invalid");
     number.classList.add("valid");
