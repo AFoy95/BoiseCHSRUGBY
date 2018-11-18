5773028f8570b7c09dd8ed3ed1f1dacd38c4d8a3
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-17 23:44:25 -0700

    added a dash of salt

--- php/User_Pages/UserSign_In.php
+++ php/User_Pages/UserSign_In.php
@@ -6,8 +6,8 @@
  $email = $_POST['email'];
  $password = $_POST['password'];
  
- echo($dao->get_User_pass($email));
-	if($dao->get_User_email($email) && password_verify($password,$dao->passkey) || $_SESSION['logged_in']){
+
+	if($dao->get_User_email($email) && $dao->get_User_pass($password) || $_SESSION['logged_in']){
 		header('Location: https://boisechsrugby.herokuapp.com/');
 		$_SESSION['isadmin']= false;
 		$_SESSION['logged_in'] = true;
--- php/php_Classes/Dao.php
+++ php/php_Classes/Dao.php
@@ -144,12 +144,14 @@ public function get_User_email($email){
 		$query->execute();
 		return reset($query->fetchAll());
 	}	
-	public function get_User_pass($email){
-	$conn=$this->getConnection();
-				$getQuery="SELECT password FROM  heroku_8d53fe00435ac54.users WHERE email = :email";
+	public function get_User_pass($password){
+		$password=password_hash($password,PASSWORD_DEFAULT);
+		$conn=$this->getConnection();
+				$getQuery="SELECT id FROM  heroku_8d53fe00435ac54.users WHERE password = :password";
                 $query=$conn->prepare($getQuery);
 				$query->bindParam(":email",$email);
                 $query->execute();
+				$pass=
                 return reset($query->fetchAll());
 	}
 	public function get_Boys_Games(){
