6db8ebafc1b155a373aa5ce9ac3d2b8e6c02ff70
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-18 18:20:19 -0700

    added a dash of salt

--- php/User_Pages/user_handler.php
+++ php/User_Pages/user_handler.php
@@ -12,7 +12,7 @@
  $pattern="^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$";
 	
 	if(filter_var($email,FILTER_VALIDATE_EMAIL)){
-		if($password_repeat != $password){
+		if($password_rep != $password){
 			$_SESSION['err']="Password verification does not match";
 		header('Location: https://boisechsrugby.herokuapp.com/php/User_Pages/UserSign_Up.php');
 		exit;
