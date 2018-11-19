086bb85fc3bc76eb2ffc9fe777f293edb49d4de4
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-18 19:20:40 -0700

    added success_fail.js

--- php/User_Pages/user_handler.php
+++ php/User_Pages/user_handler.php
@@ -13,7 +13,7 @@
 		header('Location: https://boisechsrugby.herokuapp.com/php/User_Pages/UserSign_Up.php');
 		exit;
 		}else{
-			$_SESSION["success"]="Created new User Account";
+			$_SESSION["success_fail"]="Created new User Account";
 			$dao->new_user($email,hash("sha512",$password,"YwJGQafgbRdJIC2p"));
 			$dao->passkey="YwJGQafgbRdJIC2p";
 			header('Location: https://boisechsrugby.herokuapp.com/');
@@ -21,8 +21,8 @@
 		}
 	}else{
 		
-		$_SESSION['err']="Invalid email must include an @ and .";
-		header('Location: https://boisechsrugby.herokuapp.com/php/User_Pages/UserSign_Up.php');
+		$_SESSION['success_fail']="Invalid email must include an @ and .";
+		header('Location: https://boisechsrugby.herokuapp.com/');
 		exit;
 	}
 ?>
