6e74011331e56afcd85ab3f83d7faf0aa51b5501
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-19 16:34:42 -0700

    drag and drop js

--- php/Coach_Bios/MJacob.php
+++ php/Coach_Bios/MJacob.php
@@ -51,7 +51,19 @@
             </div>
          </div>
 		<div class="content">
-		<div id="image" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+		<div id="image">
+		<form class="box" method="post" action="" enctype="multipart/form-data">
+		<div class="box__input">
+			<input class="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
+			<label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
+			<button class="box__button" type="submit">Upload</button>
+			</div>
+			<div class="box__uploading">Uploading&hellip;</div>
+			<div class="box__success">Done!</div>
+			<div class="box__error">Error! <span></span>.</div>
+			</form>
+		
+		</div>
 		<textarea>
 		</textarea>
 		</div>
