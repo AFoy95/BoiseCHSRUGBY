ee8da8df8200d23bc3bc754448ed5ea113b59372
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-18 20:36:00 -0700

    added success_fail.js

--- js/success_fail.js
+++ js/success_fail.js
@@ -2,7 +2,8 @@
 $(function() {
   $("#sign_up").click(function () {
     $("#success_fail").toggleClass("active");
-	 $('#success_fail').delay(3000).fadeOut('slow');
+	$('#success_fail').delay(3000).fadeOut('slow');
   });
- 
+	 $("#success_fail").toggleClass("hidden");
+	 $('#success_fail').delay(3000).fadeOut('slow');
 });
