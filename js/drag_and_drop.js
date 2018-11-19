f0df7f9636739c1f50775a5c5a1badee96ca80ea
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-19 16:54:15 -0700

    drag and drop js

--- js/drag_and_drop.js
+++ js/drag_and_drop.js
@@ -71,11 +71,10 @@ var $input    = $form.find('input[type="file"]'),
     $label    = $form.find('label'),
     showFiles = function(files) {
       $label.text(files.length > 1 ? ($input.attr('data-multiple-caption') || '').replace( '{count}', files.length ) : files[ 0 ].name);
-    };
 
 // ...
 
-.on('drop', function(e) {
+$input.on('drop', function(e) {
   droppedFiles = e.originalEvent.dataTransfer.files; // the files that were dropped
   showFiles( droppedFiles );
 });
@@ -85,3 +84,4 @@ var $input    = $form.find('input[type="file"]'),
 $input.on('change', function(e) {
   showFiles(e.target.files);
 });
+};
