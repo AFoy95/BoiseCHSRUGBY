00bdfeded6e1b6f4e9c2f7328e8b6494a7c8cf0e
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-18 20:14:53 -0700

    added success_fail.js

--- php/login_handles/login_handler.php
+++ php/login_handles/login_handler.php
@@ -5,14 +5,15 @@
  $conn=$dao->getConnection();
  $username = $_POST['username'];
  $password = $_POST['password'];
-	if($dao->get_admin_user($username) && $dao->get_admin_pass($password) || $_SESSION['logged_in']){
+	if($dao->get_admin_user($username) && $dao->get_admin_pass($password) || $_SESSION['isadmin']==true){
 		header('Location: https://boisechsrugby.herokuapp.com/');
-		$_SESSION['logged_in'] = true;
 		$_SESSION['isadmin']= true;
+		$_SESSION['logged_in'] = true;
 		exit;
 	}
 	$_SESSION['message'] = "Invalid Username and/or Password";
 	$_SESSION['logged_in']=false;
+	$_SESSION['isadmin']= false;
 	header('Location: https://boisechsrugby.herokuapp.com/login_handles/php/login.php');
 	exit;
 	
