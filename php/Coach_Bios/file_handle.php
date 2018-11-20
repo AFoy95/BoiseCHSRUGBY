27e1ddbdfe337c22574add2f8f02a0d10b318bf9
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-19 19:42:52 -0700

    file handling

--- php/Coach_Bios/file_handle.php
+++ php/Coach_Bios/file_handle.php
@@ -7,21 +7,19 @@ if (count($_FILES) > 0) {
   if ($_FILES["pic"]> 0) {
     throw new Exception("Error: " . $_FILES["pic"]);
 	echo("error");
-  } else {
-    $basePath = "/Users/afoy9/CS401";
-    $imagePath = "/photos/" . $_FILES["pic"];
-    if (!move_uploaded_file($_FILES["pic"], $basePath . $imagePath)) {
-      throw new Exception("File move failed");
-	  echo("move failed");
-    }
-	$dao->upload_File($_FILES["pic"]);
-  }
-		header('Location: https://boisechsrugby.herokuapp.com/');
-			session_destroy();
-			session_start();
+		} else {
+				$basePath = "/Users/afoy9/CS401";
+				$imagePath = "/photos/" . $_FILES["pic"];
+				if (!move_uploaded_file($_FILES["pic"], $basePath . $imagePath)) {
+						throw new Exception("File move failed");
+						echo("move failed");
+				}
+					$dao->upload_File($_FILES["pic"]);
+				}
+			header('Location: https://boisechsrugby.herokuapp.com/');
 			exit;
-}else{
-	header('Location: https://boisechsrugby.herokuapp.com/php/top-level/Personal_Bios.php');
-	exit;
-}
+	}else{
+		header('Location: https://boisechsrugby.herokuapp.com/php/top-level/Personal_Bios.php');
+		exit;
+	}
 ?>
