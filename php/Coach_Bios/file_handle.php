f636dea8d22b041c73f112005753f2db0dc60eb0
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-19 23:26:35 -0700

    file handling

--- php/Coach_Bios/file_handle.php
+++ php/Coach_Bios/file_handle.php
@@ -4,7 +4,7 @@ session_start();
 $imagePath = '';
 $dao=new Dao();
 $file_path=$_FILES['pic'];
-$name =$file_path['name'];
+$name =$file_path['path'];
 	$dao->upload_File($name);
 	header('Location: https://boisechsrugby.herokuapp.com/');
 	exit;
