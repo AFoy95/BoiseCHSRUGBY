306c3c6443b4dad6b47faf190b517fc8fb0b582f
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-20 23:41:49 -0700

    adding to Champioships

--- php/About_Us/Championships.php
+++ php/About_Us/Championships.php
@@ -45,6 +45,8 @@
 		</div>
 		<div class = "content">
 			<table>
+				<th>Championships</th>
+			<table>
 				<th>Boys</th><tr><td>Year</td></tr>
 								<tr><td>2008-2009</td></tr>
 								<tr><td>2009-2010</td></tr>
@@ -53,14 +55,15 @@
 				</table>
 				<table>
 				<th>Girls</th><tr><td>Year</td></tr>
-								  <td>2010-2011</td>
-								  <td>2011-2012</td>
-								  <td>2012-2013</td>
-								  <td>2013-2014</td>
-								  <td>2014-2015</td>
-								  <td>2015-2016</td>
-								  <td>2016-2017</td>
-								  <td>2017-2018</td>		  
+								  <tr><td>2010-2011</td></tr>
+								  <tr><td>2011-2012</td></tr>
+								  <tr><td>2012-2013</td></tr>
+								  <tr><td>2013-2014</td></tr>
+								  <tr><td>2014-2015</td></tr>
+								  <tr><td>2015-2016</td></tr>
+								  <tr><td>2016-2017</td></tr>
+								  <tr><td>2017-2018</td></tr>		  
+				</table>
 				</table>
 			
 		
