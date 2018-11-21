cfe8c895c0a941b063f11b8d4e8e0d1d32a5d556
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-21 11:23:42 -0700

    ajax in topnav update

--- js/topNav.js
+++ js/topNav.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-	$(".topnav a").click(function(){
-		$("a").load("#");
-    });
+		$(".topnav a").click(function(event){
+			event.preventDefault();
+		
+			$.ajax(this.href,{
+				success: function(data){
+					$('.topnav a').html($(data).find('.topnav a *'));
+			},
+			error: function(){
+				alert("Unable to load page");
+			}
+		});
+	});
 });
