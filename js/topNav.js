5af57a3e3e91d748cffca01fe150575297e9df39
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-21 11:27:03 -0700

    ajax in topnav update

--- js/topNav.js
+++ js/topNav.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		
 			$.ajax(this.href,{
 				success: function(data){
-					$('.topnav a').html($(data).find('a'));
+					$('.topnav a').html($(data).find('.topnav'));
 			},
 			error: function(){
 				alert("Unable to load page");
