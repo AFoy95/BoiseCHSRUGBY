7d2ed6d0249eaf3fe1ecb438d5f7670765f60096
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-22 20:18:32 -0700

    fixing Calendar

--- js/topNav.js
+++ js/topNav.js
@@ -1,25 +1,25 @@
 document.getElementById("main").onclick=function(event)
 {
-window.location.href = "php/top-level/main_page.php"
+window.location.href = "https://boisechsrugby.herokuapp.com/php/top-level/main_page.php"
 }
 document.getElementById("Aus").onclick=function(event)
 {
-window.location.href = "php/top-level/About_Us.php"
+window.location.href = "https://boisechsrugby.herokuapp.com/php/top-level/About_Us.php"
 }
 document.getElementById("Pb").onclick=function(event)
 {
-window.location.href = "php/top-level/Personal_Bios.php"
+window.location.href = "https://boisechsrugby.herokuapp.com/php/top-level/Personal_Bios.php"
 }
 document.getElementById("Sche").onclick=function(event)
 {
-window.location.href = "php/top-level/Schedules.php"
+window.location.href = "https://boisechsrugby.herokuapp.com/php/top-level/Schedules.php"
 }
 
 document.getElementById("Tab").onclick=function(event)
 {
-window.location.href = "php/top-level/Tables.php"
+window.location.href = "https://boisechsrugby.herokuapp.com/php/top-level/Tables.php"
 }
 document.getElementById("Adm").onclick=function(event)
 {
-window.location.href = "php/login_handles/login.php"
+window.location.href = "https://boisechsrugby.herokuapp.com/php/login_handles/login.php"
 }
