779fe8665424903a55ccb140c297df50a366b8be
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-11-27 20:52:44 -0700

    final tune ups

--- js/Modal.js
+++ js/Modal.js
@@ -18,6 +18,7 @@ picbtn.onclick = function(){
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+	card.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
